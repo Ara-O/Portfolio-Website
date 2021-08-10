f81e87558f1aca12407aafd4c6c9e3c91ff9d11f
Ara-O <oladipoeyiara@gmail.com>
2021-08-09 20:04:52 -0400

    Worked on first section on page ( navbar and hero section )

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="blend">
+      <img src="../assets/background.jpg" class="bg-img" />
+    </div>
+    <div class="hero--header">
+      <div class="arrange">
+        <h5 class="intro">Hi, I am</h5>
+        <h3 class="name">Eyiara Oladipo</h3>
+        <h5 class="my--desc">
+          Freshman at the University of Detroit Mercy studying Computer Science
+        </h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="../styles/home.scss" lang="scss"></style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav id="nav">
+    <ul class="navbar">
+      <li>About Me</li>
+      <li>My Skills</li>
+      <li>Contact Me</li>
+    </ul>
+  </nav>
+</template>
+
+<style src="../styles/navbar.scss" lang="scss"></style>
+
+<style scoped></style>
