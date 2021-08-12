cc720a576e56c25f8b1400d9ae1c7a588adcf987
Ara-O <oladipoeyiara@gmail.com>
2021-08-12 19:53:52 -0400

    Scrapped horizontal scrolling feature and started projects section

--- src/components/home.vue
+++ src/components/home.vue
@@ -50,99 +50,107 @@
         experience in firebase and squarespace
       </h4>
     </div>
-    <div class="divider-div">
-      <div class="divider"></div>
-    </div>
+    <div class="divider-div"><div class="divider"></div></div>
     <div class="aboutme--right">
       <h2 style="font-size: 39px">TL;DR</h2>
-      <h4 style="font-size: 18px; font-weight: 100">
-        Name: Eyiara "Ara" Oladipo
-      </h4>
-      <h4 style="font-size: 18px; font-weight: 100">Age: 16 years old</h4>
-      <h4 style="font-size: 18px; font-weight: 100">
-        SKills: HTML | CSS | JS | Vue.js | Firebase
-      </h4>
+      <h4 class="tldr">Name: Eyiara "Ara" Oladipo</h4>
+      <h4 class="tldr">Age: 16 years old</h4>
+      <h4 class="tldr">Skills: HTML | CSS | JS | Vue.js | Firebase</h4>
+      <h4 class="tldr">Interests: UX/UI design, Front End development</h4>
+      <br />
+      <mainbtn
+        bgcolor="linear-gradient(126deg, black, rgb(102 102 102))"
+        textcolor="white"
+        btntext="Resume"
+      ></mainbtn>
+      <mainbtn
+        bgcolor="linear-gradient(136deg, #790000, #da0000)"
+        textcolor="white"
+        btntext="View Contact Info"
+        style="margin-left: 20px"
+      ></mainbtn>
+    </div>
+  </section>
+  <section class="projects">
+    <h2 style="margin-top: 0px">Projects</h2>
+    <div class="bothprojects">
+      <section class="project1">
+        <div class="iframes">
+          <iframe
+            width="1181px"
+            height="650px"
+            src="https://animesfinder.netlify.app/"
+            style="transform: scale(0.75) translate(-195px, -80px)"
+          ></iframe>
+        </div>
+        <div class="projectdescr"></div>
+      </section>
+      <section class="project2"></section>
     </div>
   </section>
 </template>
-
 <script>
 import mainbtn from "../helpers/button.vue";
 export default {
   name: "Home",
-  components: {
-    mainbtn,
-  },
+  components: { mainbtn },
   data() {
     return {};
   },
+
   methods: {
-    changeIndicator(prev, current) {
-      document.querySelector("." + prev).style.background =
-        "rgb(160, 160, 160)";
+    changeIndicator(current) {
+      document.querySelectorAll(".indicator").forEach((circle) => {
+        circle.style.background = "rgb(160, 160, 160)";
+      });
       document.querySelector("." + current).style.background = "#da0000";
     },
   },
 
   mounted() {
-    const aboutmesect = document.querySelector(".aboutme");
-    let options = {
-      threshold: 0.5,
-    };
-    const that = this;
-
-    let observer = new IntersectionObserver(function (e) {
+    const main = document.querySelector(".navbar");
+    let observer3 = new IntersectionObserver(function (e) {
       if (e[0].isIntersecting) {
-        // document.querySelector(".mainbtn-1").classList.add("minimized");
-        that.changeIndicator("one", "two");
-      }
-      if (!e[0].isIntersecting) {
-        // document.querySelector(".mainbtn-1").classList.remove("minimized");
-        that.changeIndicator("two", "one");
+        that.changeIndicator("one");
       }
     }, options);
-    observer.observe(aboutmesect);
 
+    observer3.observe(main);
+
+    //----------
+
+    const aboutmesect = document.querySelector(".aboutme");
+    let options = { threshold: 0.5 };
+    const that = this;
+    let observer = new IntersectionObserver(function (e) {
+      if (e[0].isIntersecting) {
+        that.changeIndicator("two");
+      }
+    }, options);
+
+    observer.observe(aboutmesect);
     document.querySelectorAll(".indicator").forEach((circle) => {
       circle.addEventListener("click", function () {
         console.log(circle.dataset.scrollto);
       });
     });
+
+    // --------
+
+    const projects = document.querySelector(".projects");
+    let options2 = { threshold: 0.5 };
+    let observer2 = new IntersectionObserver(function (e) {
+      if (e[0].isIntersecting) {
+        that.changeIndicator("three");
+      }
+
+      if (!e[0].isIntersecting) {
+        that.changeIndicator("two");
+      }
+    }, options2);
+
+    observer2.observe(projects);
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style src="../styles/home.scss" lang="scss"></style>
-
-<style scoped>
-/* .mainbtn-1 {
-  cursor: pointer;
-  height: 46px;
-  font-size: 17px;
-  border: none;
-  font-family: "Agency FB";
-  position: sticky;
-  top: 87px;
-  left: 998px;
-  z-index: 900;
-  padding: 0px 25px;
-  background: white;
-  color: black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: auto;
-  flex: 0 auto;
-  transition: all 1s ease;
-  text-align: center;
-} */
-
-/* .minimized {
-  transform: translate3d(26vw, 80vh, 0px) scale3d(0.85, 0.85, 1) rotateX(0deg)
-    rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
-  transform-style: preserve-3d;
-  position: fixed;
-  bottom: 0;
-} */
-</style>
